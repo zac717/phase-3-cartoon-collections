dbe43347c7e96e669303af8c9ab390ac90349a27
Isaac Ongera <isaacongera717@gmail.com>
2023-03-05 16:09:41 +0300

    completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  array.each.with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array = array.collect do |name|
+    "#{name.capitalize}!"
+  end
+  array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  status = array.any? do |calls|
+    calls.length > 4
+  end
+  status
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese = array.find do |type|
+    cheese_types.include?(type)
+  end
+  cheese
 end
+
+find_the_cheese(["garlic", "rosemary", "bread"])
